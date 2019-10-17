1a15113308ba4158cc20936925b01b025be2a562
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-16 23:58:26 -0500

    Se arreglo la ventanaRegistro, para que se pudiera ver

--- src/cocochat/CocoChat.java
+++ src/cocochat/CocoChat.java
@@ -16,8 +16,10 @@ public class CocoChat {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        VentanaLogin ventanaLogin= new VentanaLogin();
-        ventanaLogin.show();
+        VentanaRegistro a = new VentanaRegistro();
+        a.show();
+        //VentanaLogin ventanaLogin= new VentanaLogin();
+        //ventanaLogin.show();
     }
     
     
--- src/cocochat/VentanaRegistro.java
+++ src/cocochat/VentanaRegistro.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package login;
+package cocochat;
 
 import java.awt.Image;
 import javax.swing.GroupLayout;
