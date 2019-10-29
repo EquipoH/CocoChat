eb9638669691fba4e975d84ff01f959958b6bc81
bonnesbaby14 <bonnesbaby14@192.168.0.13>
2019-10-29 00:45:47 -0700

    conexion con el server

--- src/cocochat/CocoChat.java
+++ src/cocochat/CocoChat.java
@@ -5,7 +5,11 @@
  */
 package cocochat;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,30 +34,32 @@ public class CocoChat {
 
             
          
-            cliente = new Socket ("192.168.1.106", 1234);
+            cliente = new Socket ("192.168.0.5", 1234);
 
-                   
-    
+                   DataInputStream entrada=new DataInputStream( cliente.getInputStream());
+                   DataOutputStream salida=new DataOutputStream(cliente.getOutputStream());
             //resultado = cliente.getInputStream().read(); //toma un ascii
             //System.out.println(""+resultado);
             //cliente.getOutputStream().write('7');
-            cliente.getInputStream().read(recepcion);
-            msg = new String(recepcion);
-            System.out.println(msg);
+         
            // cliente.getOutputStream().write(msg.getBytes());
             if(cliente.isConnected()){
                  System.out.println("Se conecto correctamente");
             }else{
                 System.out.println("Ocurrio un error para conectarse");
             }
-           
+             String dato;
+           dato=entrada.readUTF();
+            System.out.println(dato);
+            
            
            VentanaLogin a = new VentanaLogin(cliente);
                  a.show();
+                 
+                 
            
            
            
-            cliente.close();
         } catch (IOException ex) {
             Logger.getLogger(CocoChat.class.getName()).log(Level.SEVERE, null, ex);
         }
